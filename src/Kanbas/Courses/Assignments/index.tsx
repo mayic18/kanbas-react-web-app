9c602f6726d00df4c6491868e758f428c2bdf6c2
unknown <ma.yic@northeastern.edu>
2024-12-06 18:50:38 -0500

    a6

--- src/Kanbas/Courses/Assignments/index.tsx
+++ src/Kanbas/Courses/Assignments/index.tsx
@@ -1,8 +1,11 @@
 import { FaPlus, FaCheckCircle } from "react-icons/fa";
-import { FaMagnifyingGlass } from "react-icons/fa6";
 import { BsGripVertical } from "react-icons/bs";
 import { HiDotsVertical } from "react-icons/hi";
 import { TiArrowSortedDown } from "react-icons/ti";
+import { MdAssignment } from "react-icons/md";
+import { FaTrash } from "react-icons/fa";
+import { FaSearch } from 'react-icons/fa';
+import { IoEllipsisVertical } from "react-icons/io5";
 import { useSelector, useDispatch } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { useParams } from "react-router";
@@ -11,10 +14,7 @@ import { RootState } from "../../store";
 import * as assignmentsClient from "./client";
 import { setAssignments } from "./reducer";
 import { useEffect } from "react";
-import { MdAssignment } from "react-icons/md";
-import { FaTrash } from "react-icons/fa";
-import { FaSearch } from 'react-icons/fa';
-import { IoEllipsisVertical } from "react-icons/io5";
+
 interface Assignment {
   _id: string;
   course: string;
@@ -32,12 +32,11 @@ export default function Assignments() {
   const dispatch = useDispatch();
   const { currentUser } = useSelector((state: any) => state.accountReducer);
   const isFaculty = currentUser?.role === "FACULTY";
-  // Retrieve assignments from the Redux store via assignmentsReducer
   const assignments = useSelector((state: RootState) =>
     state.assignmentReducer.assignments.filter((assignment: Assignment) => assignment.course === cid)
   );
 
-  const fetchAssignments = async () => { 
+  const fetchAssignments = async () => {
     const assignments = await assignmentsClient.findAssignmentsForCourse(cid as string);
     dispatch(setAssignments(assignments));
   }
@@ -52,7 +51,7 @@ export default function Assignments() {
     }
   };
 
-  
+
 
 
   return (
@@ -82,7 +81,7 @@ export default function Assignments() {
           </div>
         )}
       </div>
-      
+
       <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
         <div className="wd-title p-3 ps-2 bg-secondary">
           <BsGripVertical className="me-2 fs-3" />
@@ -93,7 +92,7 @@ export default function Assignments() {
         </div>
 
         <ul className="wd-lessons list-group rounded-0">
-          {assignments.map((assignment:Assignment) => (
+          {assignments.map((assignment: Assignment) => (
             <li key={assignment._id} className="wd-assignment-list-item list-group-item d-flex align-items-center mb-3">
               <BsGripVertical className="me-2 fs-3" />
               <MdAssignment className="me-2 fs-3" />
@@ -102,15 +101,15 @@ export default function Assignments() {
                   className="wd-assignment-link fw-bold text-decoration-none text-dark"
                   to={`/Kanbas/Courses/${cid}/Assignments/${assignment._id}`}
                 >
-                   <span style={{ fontSize: '18px', fontWeight: 'bold' }}>{assignment.title}</span>
-                   
-                <p className="text-muted">
-                  <span style={{ fontSize: '16px', color: 'red' }}>Multiple Modules |</span>
-                  <span style={{ fontSize: '16px', color: 'black' }}>
-                    <b>Due</b> {assignment.dueDate} || Not available until {assignment.availableDate} | {assignment.points} pts
-                  </span>
-                 
-                </p>
+                  <span style={{ fontSize: '18px', fontWeight: 'bold' }}>{assignment.title}</span>
+
+                  <p className="text-muted">
+                    <span style={{ fontSize: '16px', color: 'red' }}>Multiple Modules |</span>
+                    <span style={{ fontSize: '16px', color: 'black' }}>
+                      <b>Due</b> {assignment.dueDate} || Not available until {assignment.availableDate} | {assignment.points} pts
+                    </span>
+
+                  </p>
                 </Link>
               </div>
               <FaCheckCircle className="text-success me-3" />
